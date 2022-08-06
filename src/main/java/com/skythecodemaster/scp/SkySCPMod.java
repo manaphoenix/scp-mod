600f41417a8d1079567e036f5d3a320f3c5240c2
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-08-06 00:31:34 -0400

    make compile

--- src/main/java/com/skythecodemaster/scp/SkySCPMod.java
+++ src/main/java/com/skythecodemaster/scp/SkySCPMod.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(SkySCPMod.MOD_ID)
+@Mod("skysscp")
 public class SkySCPMod
 {
     // Directly reference a slf4j logger
