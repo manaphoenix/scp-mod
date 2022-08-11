8b36305ab597a64e75f0a8066256dc331c4874bb
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-08-10 23:30:12 -0400

    stuff

--- src/main/java/com/skythecodemaster/scp/common/blockentities/LightDoorOldBlockEntity.java
+++ src/main/java/com/skythecodemaster/scp/common/blockentities/LightDoorOldBlockEntity.java
@@ -1,11 +1,16 @@
 package com.skythecodemaster.scp.common.blockentities;
 
+import com.mojang.logging.LogUtils;
+import com.skythecodemaster.scp.common.blocks.LightDoorOld;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.slf4j.Logger;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -16,7 +21,10 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import static com.skythecodemaster.scp.common.setup.BlockEntityTypes.LIGHT_DOOR_OLD_TILE;
 
-public class LightDoorOldBlockEntity extends BlockEntity implements IAnimatable {
+public class LightDoorOldBlockEntity extends BlockEntity implements IAnimatable, BlockEntityTicker<LightDoorOldBlockEntity> {
+  public LightDoorOld attachedDoor;
+  private static final Logger LOGGER = LogUtils.getLogger(); // Collect a logger
+  
   public LightDoorOldBlockEntity(BlockPos pos, BlockState state) {
     super(LIGHT_DOOR_OLD_TILE.get(),pos,state);
   }
@@ -37,5 +45,14 @@ public class LightDoorOldBlockEntity extends BlockEntity implements IAnimatable 
     return this.factory;
   }
   
-
+  public static void tick(Level level, BlockPos pos, BlockState state, LightDoorOldBlockEntity blockEntity) {
+    LOGGER.info("tick called");
+    LOGGER.info(String.valueOf(level.hasNeighborSignal(pos)));
+    if (level.hasNeighborSignal(pos) || level.hasNeighborSignal(pos.above())) {
+      this.attachedDoor.state = true;
+    } else {
+      this.attachedDoor.state = false;
+    }
+    this.attachedDoor.doState();
+  }
 }
--- src/main/java/com/skythecodemaster/scp/common/blocks/LightDoorOld.java
+++ src/main/java/com/skythecodemaster/scp/common/blocks/LightDoorOld.java
@@ -1,11 +1,16 @@
 package com.skythecodemaster.scp.common.blocks;
 
+import com.mojang.logging.LogUtils;
+import com.skythecodemaster.scp.common.blockentities.LightDoorOldBlockEntity;
 import com.skythecodemaster.scp.common.setup.BlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -15,6 +20,8 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -26,6 +33,11 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class LightDoorOld extends DirectionalBlock implements EntityBlock, IAnimatable {
   
+  public boolean state = false; // true for open, false for close
+  private boolean lastState = false; // This is the previous state of the state, so we dont spam stuffs
+  
+  private static final Logger LOGGER = LogUtils.getLogger(); // Collect a logger
+  
   private static final VoxelShape SHAPE_NORTH = Shapes.box(0,0,0.375f,1,2,0.625f);
   private static final VoxelShape SHAPE_SOUTH = SHAPE_NORTH;
   private static final VoxelShape SHAPE_EAST = Shapes.box(0.375f,0,0,0.625f,2,1);
@@ -36,7 +48,13 @@ public class LightDoorOld extends DirectionalBlock implements EntityBlock, IAnim
   }
   
   public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-    return BlockEntityTypes.LIGHT_DOOR_OLD_TILE.get().create(pos,state);
+    BlockEntity x = BlockEntityTypes.LIGHT_DOOR_OLD_TILE.get().create(pos,state);
+    ((LightDoorOldBlockEntity) x).attachedDoor = this;
+    return x;
+  }
+  
+  public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+    return type == BlockEntityTypes.LIGHT_DOOR_OLD_TILE.get() ? LightDoorOldBlockEntity::tick : null;
   }
   @Override
   public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
@@ -48,6 +66,14 @@ public class LightDoorOld extends DirectionalBlock implements EntityBlock, IAnim
     event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.skysscp.scp_light_door_old"));
     return PlayState.CONTINUE;
   }
+  
+  public void doState() {
+    if (lastState != state) {
+      lastState = state;
+      LOGGER.info("Door state: %s".format(String.valueOf(state)));
+    }
+  }
+  
   @Override
   public void registerControllers(AnimationData data) {
     data.addAnimationController(new AnimationController<LightDoorOld>(this,"controller",0,this::predicate));
@@ -58,9 +84,11 @@ public class LightDoorOld extends DirectionalBlock implements EntityBlock, IAnim
     return this.factory;
   }
   
+  @Nullable
   @Override
   public BlockState getStateForPlacement(BlockPlaceContext context) {
-    return this.defaultBlockState().setValue(BlockStateProperties.FACING, context.getNearestLookingDirection().getOpposite());
+    return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+    //return this.defaultBlockState().setValue(BlockStateProperties.FACING, context.getNearestLookingDirection().getOpposite());
   }
   
   @Override
@@ -70,12 +98,16 @@ public class LightDoorOld extends DirectionalBlock implements EntityBlock, IAnim
   
   @Override
   public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pPos, CollisionContext context) {
-    return switch (state.getValue(BlockStateProperties.FACING)) {
-      case NORTH, DOWN, UP -> SHAPE_NORTH;
-      case SOUTH -> SHAPE_SOUTH;
-      case EAST -> SHAPE_EAST;
-      case WEST -> SHAPE_WEST;
-    };
+    if (this.state) {
+      return Shapes.box(0,0,0,0,0,0);
+    } else {
+      return switch (state.getValue(BlockStateProperties.FACING)) {
+        case NORTH, DOWN, UP -> SHAPE_NORTH;
+        case SOUTH -> SHAPE_SOUTH;
+        case EAST -> SHAPE_EAST;
+        case WEST -> SHAPE_WEST;
+      };
+    }
     //return Shapes.box(0,0,0.375f,1,2,0.625f);
   }
 }
